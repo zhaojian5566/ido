5a09944e39e74f2aa90825a23825eb02c1cb0c4b
zhaojian5566 <564450564@qq.com>
2019-02-18 10:05:29 +0800

    注释

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // 版本控制系统:VCS, Version Control System
+        // 1.集中式版本控制系统: SVN,Subversion
+        // 2.分布式版本控制系统: Git,
+
+        // SVN服务器: taoCode, SVNChina
+        // Git服务器: gitee, github
+
+        // Git使用
+        // Git安装
+        // 下载地址:https://git-scm.com/
+
+        // 初始化版本库
+        // git init
+        // 注: 当前文件夹,有文件要进行版本控制
+
+        // 查看当前版本库的状态
+        // git status
+
+        // 执行某个文件进行版本控制
+        // git add
+    }
+}
