86d7e213fe2d2e29112743ae1850113ee59631d3
zhaojian5566 <564450564@qq.com>
2019-02-18 10:17:38 +0800

    我要提交

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,28 @@ public class Main {
         // git status
 
         // 执行某个文件进行版本控制
-        // git add
+        // git add 文件
+
+        // 红色: 未进行版本控制的文件
+        // 绿色: 新增文件
+
+        // git信息配置
+        // git config --global user.name "姓名"
+        // git config --global user.email "邮箱"
+
+        // 删除git的配置信息
+        // git config --global --unset user.email
+
+        // 查看git信息配置
+        // git config --global --list
+
+        // 注:
+        // --system: 系统
+        // --global: 当前用户
+        // --local: 当前仓库
+
+        // 提交修改
+        // git commit -m "注释"
+
     }
 }
