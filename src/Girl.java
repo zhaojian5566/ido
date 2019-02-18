5f32bd7a0fcdd77237cf8cf43fa8ddcb7c3538f4
zhaojian5566 <564450564@qq.com>
2019-02-18 11:29:14 +0800

    提交

--- src/Girl.java
+++ src/Girl.java
@@ -1,6 +1,36 @@
 public class Girl {
-    public static void main(String[] args) {
-        // 提交第二次
+    private String name;
+    private int age;
 
+    public Girl() {
+    }
+
+    public Girl(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
